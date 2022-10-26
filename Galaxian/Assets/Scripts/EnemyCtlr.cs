f7e1d77a88bc2614cdf6bceea75e1f25a80b1c3f
Ryota-ika <daiouga@i.softbank.jp>
2022-10-26 23:42:04 +0900

    10.26

--- Galaxian/Assets/Scripts/EnemyCtlr.cs
+++ Galaxian/Assets/Scripts/EnemyCtlr.cs
@@ -12,15 +12,14 @@ public class EnemyCtlr : MonoBehaviour {
 
     // Update is called once per frame
     void Update( ) {
-        //プレイヤーの方向を向く
-        Quaternion look_rotation=Quaternion.FromToRotation(transform.position,target.transform.position);
-        transform.rotation = look_rotation;
-        //look_rotation.z=0;
-        //look_rotation.x=0;
+        //プレイヤーの向きを変える
+        //transform.positionからtarget.transform.positionの方向に向きを変える
+        Quaternion look_rotation =Quaternion.FromToRotation(transform.position,target.transform.position);
+        //transform.rotation = look_rotation;
         ////2点間を滑らかに移動させるための関数
-        //transform.rotation=Quaternion.Lerp(transform.rotation,look_rotation,0.1f);
+        transform.rotation=Quaternion.Lerp(transform.rotation,look_rotation,0.1f);
         
-        Vector3 p =new Vector3(0f,-0.05f,0f);
+        //Vector3 p =new Vector3(0f,-0.05f,0f);
         //targetに向かって前進
         //相対的
         //transform.Translate(p);
