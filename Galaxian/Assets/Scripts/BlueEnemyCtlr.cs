dc0379c64ede76381833d257df6dd710ceb1b994
Ryota-ika <daiouga@i.softbank.jp>
2022-11-03 15:55:53 +0900

    11.3

--- Galaxian/Assets/Scripts/BlueEnemyCtlr.cs
+++ Galaxian/Assets/Scripts/BlueEnemyCtlr.cs
@@ -2,30 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlueEnemyCtlr : MonoBehaviour
+public class BlueEnemyCtlr : AllEnemyCtlr
 {
     public GameObject explosion;
     // Start is called before the first frame update
-    void Start()
-    {
+    //void Start()
+    //{
 
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "PlayerGun")
-        {
-            //Destroy(collision.gameObject);
-            collision.gameObject.SetActive(false);
-            Destroy(this.gameObject);
-            //instantiate‚ğg‚¢‚·‚¬‚é‚Æˆ—‚ª‚¨‚à‚­‚È‚é‚Ì‚Åpooling‚É•ÏX
-            //Instantiate(explosion,transform.position,Quaternion.identity);
-        }
-    }
+    //}
 }
